2ab326c6ee362fd62821050389f100b347dc4bf3
Jakub <jakubwolak.123@gmail.com>
2021-12-13 18:36:00 +0100

    images and login page template

---
+++ lib/login_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            width: w,
+            height: h * 0.3,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "img/loginimg.png"
+                ),
+                fit: BoxFit.cover,
+              )
+            )
+          )
+        ],
+      )
+    );
+  }
+}
